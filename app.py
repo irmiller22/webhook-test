a5a422bac380846c91bbb7250f3ded55a7c2d053
Ian Miller <irmiller22@gmail.com>
2015-08-17 14:09:18 -0400

    added a bunch of print statements

--- app.py
+++ app.py
@@ -17,21 +17,36 @@ def foo():
     if sha_name != 'sha1':
         print "sha name doesnt equal sha1"
 
+    print 'got past sha1'
+
     mac = hmac.new(str('secret'), msg=request.data, digestmod=sha1)
+
+    print 'got pass mac setting'
 
     print mac.hexdigest()
 
+    print 'before the hmac conditional'
+
+    print ('Event: {}'
+           .format(request.headers.get('X-GitHub-Event') == u'push'))
+
     if (hmac.compare_digest(str(mac.hexdigest()), signature) and
             request.headers.get('X-GitHub-Event') == u'push'):
+
+        print 'Got past the hexdigest and push event conditional'
 
         api_data = json.loads(request.data)
         print api_data
         print 'SECRET WAS A SUCCESS!'
 
+        print 'got past json.loads(request.data)'
+
         success_message = {
             'status': 200,
             'message': "Github Webhook: success!"
         }
+
+        print 'setting response'
 
         response = jsonify(success_message)
         response.status_code = 200
@@ -41,10 +56,17 @@ def foo():
             'message': "Github Webhook: failed!"
         }
 
+        print 'In the failure section'
+
         response = jsonify(failure_message)
         response.status_code = 400
 
+    print response
     return response
 
 if __name__ == '__main__':
     app.run()
+
+
+signature = 'ba585f639e2dcf8532e44d467be2eeb15ed196dc'
+hexdigest = 'ba585f639e2dcf8532e44d467be2eeb15ed196dc'
