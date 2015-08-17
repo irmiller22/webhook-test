16797908369af4ba3fc698dc992d51c982a45f98
Ian Miller <irmiller22@gmail.com>
2015-08-17 13:44:46 -0400

    implementing secret

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify
 import json
+import hmac
+from hashlib import sha1
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
 def foo():
-    message = {
+    success_message = {
         'status': 200,
         'message': "Github Webhook: success!"
     }
@@ -14,11 +16,33 @@ def foo():
     response = jsonify(message)
     response.status_code = 200
 
-    if request.headers.get('X-GitHub-Event') == u'push':
+    sha_name, signature = requests.headers.get('X-Hub-Signature').split('=')
+    if sha_name != 'sha1':
+        print "sha name doesnt equal sha1"
+
+    mac = hmac.new(str('secret'), msg=request.data, digestmod=sha1)
+
+    if hmac.compare_digest(str(mac.hexdigest()), str('secret')) and
+        request.headers.get('X-GitHub-Event') == u'push':
+
         api_data = json.loads(request.data)
         print api_data
+        print 'SECRET WAS A SUCCESS!'
 
-    return response
+        success_message = {
+            'status': 200,
+            'message': "Github Webhook: success!"
+        }
+
+        response = jsonify(message)
+        response.status_code = 200
+
+        return response
+    else:
+        failure_message = {
+            'status': 400,
+            'message': "Github Webhook: failed!"
+        }
 
 if __name__ == '__main__':
     app.run()
